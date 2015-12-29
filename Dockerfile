60ecc1bf6417c2087f187c9d7c0a74f1baf3a361
tim <tfitz@umd.edu>
2015-12-28 16:48:29 -0800

    change working directory

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN apt-get update && apt-get install -y \
     sdaps 
 
 
-WORKDIR "/sdaps"
+WORKDIR "/workdir"
 
 ENTRYPOINT ["sdaps"]
 
--- run_sdaps.sh
+++ run_sdaps.sh
@@ -3,12 +3,9 @@
 cwd=$( pwd )
 
 docker run \
-       -it \
-       -v $cwd:/sdaps \
+       -v $cwd:/workdir \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -e DISPLAY=$DISPLAY \
-       -e uid=$(id -u) \
-       -e gid=$(id -g) \
        sdaps1 \
        "$@"
 
